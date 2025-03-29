ac5a064da9d950860ee358e58a7e26f8d5fc6150
Irena Daneva <de.luxe9997@gmail.com>
2025-03-30 00:53:59 +0200

    update

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ function changeBackground() {
     }
     clickCount++;
 } 
+
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+function formValidate() {
+    if(document.getElementById("name").value.length == 0) {
+    alert("Name field is empty");
+    }
+    else {
+        if(document.getElementById("email").value.match(mailformat)) {
+            if(document.getElementById("message").value.length == 0){
+                alert("No message included");
+            }
+            else alert("Form submitted.");
+        }
+        else alert("Incorrect email address");
+    }
+}
