dd536dd2290586c80af6411f3098a62925019849
Irena Daneva <de.luxe9997@gmail.com>
2025-03-31 00:34:17 +0300

    contact update

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ let clickCount = 0;
 function changeBackground() {
     if (clickCount%2 == 0) {
         document.body.style.backgroundColor = "rgb(126, 68, 96)";
-        if (document.title === 'Portfolio')
-        {
+        if (document.title === 'Portfolio') {
             document.getElementById("welcome").style.color = "white";
         }
         else if(document.title === 'About Me') {
@@ -13,35 +12,29 @@ function changeBackground() {
         else if(document.title === 'Education') {
             document.getElementById("educationPage").style.color = "white";
         }
-        
-        /*cument.getElementById("aboutText").style.color = "white";
-        document.getElementById("hobbiesText").style.color = "white";
-        document.getElementById("contactText").style.color = "white";*/
+        else if(document.title === 'Contact Me') {
+            document.getElementById("contact").style.color = "white";
+        }
         document.getElementById("changeColorBtn").innerText = "Background changed!"; 
     }
     else {
         document.body.style.backgroundColor = "white";
-        if (document.title === 'Portfolio')
-            {
-                document.getElementById("welcome").style.color = "black";
-            }
-            else if(document.title === 'About Me') {
-                document.getElementById("aboutText").style.color = "black";
-            }
-            else if(document.title === 'Education') {
-                document.getElementById("educationPage").style.color = "black";
-            }
-        /*cument.getElementById("aboutText").style.color = "black";
-        document.getElementById("hobbiesText").style.color = "black";
-        document.getElementById("contactText").style.color = "black";*/
+        if (document.title === 'Portfolio') {
+            document.getElementById("welcome").style.color = "black";
+        }
+        else if(document.title === 'About Me') {
+            document.getElementById("aboutText").style.color = "black";
+        }
+        else if(document.title === 'Education') {
+            document.getElementById("educationPage").style.color = "black";
+        }
+        else if(document.title === 'Contact Me') {
+            document.getElementById("contact").style.color = "black";
+        }
         document.getElementById("changeColorBtn").innerText = "Change background"; 
     }
     clickCount++;
 } 
-
-/*function changeColorBtn() {
-    document.get.style.backgroundColor="rgb(126, 68, 96)";
-}*/
 
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
