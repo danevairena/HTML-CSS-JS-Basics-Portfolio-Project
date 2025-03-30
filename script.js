74522610949722a808f4d56268e33a6ab643ad0e
Irena Daneva <de.luxe9997@gmail.com>
2025-03-30 13:30:53 +0300

    update

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ let clickCount = 0;
 function changeBackground() {
     if (clickCount%2 == 0) {
         document.body.style.backgroundColor = "rgb(126, 68, 96)";
-        document.getElementById("aboutText").style.color = "white";
+        /*cument.getElementById("aboutText").style.color = "white";
         document.getElementById("hobbiesText").style.color = "white";
-        document.getElementById("contactText").style.color = "white";
+        document.getElementById("contactText").style.color = "white";*/
         document.getElementById("changeColorBtn").innerText = "Background changed!"; 
     }
     else {
         document.body.style.backgroundColor = "white";
-        document.getElementById("aboutText").style.color = "black";
+        /*cument.getElementById("aboutText").style.color = "black";
         document.getElementById("hobbiesText").style.color = "black";
-        document.getElementById("contactText").style.color = "black";
+        document.getElementById("contactText").style.color = "black";*/
         document.getElementById("changeColorBtn").innerText = "Change background"; 
     }
     clickCount++;
 } 
+
+/*function changeColorBtn() {
+    document.get.style.backgroundColor="rgb(126, 68, 96)";
+}*/
 
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
